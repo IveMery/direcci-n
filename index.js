0c9ab455a98f578d78ad273e2a8850d20a02e1c5
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:12:48 -0400

    direccion

---
+++ index.js
@@ -0,0 +1,5 @@
+// Direccion completa
+alert(" Hola! ingrese su direccion")
+let direccion = prompt("Ingrese calle") + " , " +  prompt("Ingrese el  número") + " , " +  prompt("Ingrese departamento") + " , " +
+prompt("Ingrese código postal") + " , " +  prompt("Ingrese Ciudad") + " , " +  prompt("Ingrese país ") 
+alert ( `La dirección que ha ingresado es: " ${direccion}  " .`)
